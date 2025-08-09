f2d80ca414e36f72a669ecdd0c29dd7ca00cfc6f
Dhinakaran Sekar <dhinakaran15022000@gmail.com>
2025-08-09 15:46:24 +0530

    header section added

--- event_booking_react/src/App.jsx
+++ event_booking_react/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import LoginPage from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Navigate to="/login" replace />} />
+				<Route path="/" element={<Header />} />
 				<Route path="/login" element={<LoginPage />} />
 			</Routes>
 		</Router>
---
+++ event_booking_react/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell as faBellRegular, faUserCircle as faUserCircleRegular } from "@fortawesome/free-regular-svg-icons";
+
+export default function Header() {
+    return (
+        <header className="bg-[#474554] shadow-md flex items-center justify-between py-[15px] px-[30px] gap-[20px]">
+            <div className="font-[500] text-[27px] text-[whitesmoke]">
+                EventHub<span className="text-black text-[18px]">.com</span>
+            </div>
+
+            <div className="w-1/2 relative">
+                <input
+                    type="text"
+                    placeholder="Search events..."
+                    className="box-border w-full p-[10px] pr-[40px] border border-gray-300 rounded-[3em] focus:outline-none focus:ring-2 focus:ring-[#cfcfcf] text-[16px]"
+                />
+                <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" className="absolute right-[10px] top-1/2 -translate-y-1/2 text-gray-500 cursor-pointer text-[#626262] text-[17px]" />
+            </div>
+
+            <div className="flex items-center gap-[10px] text-gray-600">
+                <FontAwesomeIcon icon={faBellRegular} size="lg" className="cursor-pointer hover:text-[#aca7cb] text-[whitesmoke]" />
+                <FontAwesomeIcon icon={faUserCircleRegular} size="lg" className="cursor-pointer hover:text-[#aca7cb] text-[whitesmoke]" />
+            </div>
+        </header>
+    );
+}
