1f8dfd522e135ca2c0e14d34c135171338cc660a
Dhinakaran Sekar <dhinakaran15022000@gmail.com>
2025-08-20 12:39:23 +0530

    put method added

--- event_booking_drf/api/urls.py
+++ event_booking_drf/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HallListAPIView
+from .views import HallListAPIView, HallUpdateAPIView
 
 urlpatterns = [
     path('event-hall/', HallListAPIView.as_view(), name="hall-list"),
+    path('event-hall/<str:hall_id>/', HallUpdateAPIView.as_view(), name="hall-update"),
 ]
--- event_booking_drf/api/views.py
+++ event_booking_drf/api/views.py
@@ -19,9 +19,33 @@ class HallListAPIView(APIView):
                 {"error": "This email is already registered."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+        
+        phone = request.data.get('phone')
+        if EventHall.objects.filter(phone=phone):
+            return Response(
+                {"error": "This phone number is already registered."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         serializer = EventHallSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"message": "Event Hall Details Added Successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class HallUpdateAPIView(APIView):
+    
+    def get_object(self, hall_id):
+        try:
+            return EventHall.objects.get(hall_id=hall_id)
+        except EventHall.DoesNotExist:
+            return Response({"error": f"This hall id - {hall_id} does not exist."}, status=status.HTTP_404_NOT_FOUND)
+
+    @swagger_auto_schema(request_body=EventHallSerializer)
+    def put(self, request, hall_id):
+        event_hall = self.get_object(hall_id)
+        serializer = EventHallSerializer(event_hall, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": f"Hall id - {hall_id} details updated."}, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
